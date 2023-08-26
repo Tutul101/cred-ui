c40b31381d190411cef0a90c4caa7c0b9335a779
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2023-08-26 22:18:46 +0530

    add feel special branch

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import HeroSection from "../../components/herosection";
+<<<<<<< Updated upstream
 
 const HomePage = () => {
   return <HeroSection />;
+=======
+import Header from "../../components/common/header";
+import ProductShowCase from "../../components/productShowcase";
+import FeelSpecial from "../../components/feelSpecial";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <ProductShowCase />
+      <FeelSpecial />
+    </>
+  );
+>>>>>>> Stashed changes
 };
 
 export default HomePage;
